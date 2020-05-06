ed5aba2744a7be8e29da8bc881007f7946f2374d
Parasar Thulasiram <p.thulasiram@mail.utoronto.ca>
2020-05-06 15:33:06 -0400

    modified main to output half of average by defining constant

--- main.c
+++ main.c
@@ -4,10 +4,9 @@
 
 int main() {
     double arr[] = {1.0, 2.0, 3.0, 4.0};
-
-    double result = average(4, arr);
-
-    printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
+    double constant = 2.0;
+    double result = average(4, arr)/constant;
+    printf("Half of the average of 1, 2, 3 and 4 is: %.4f\n", result);
     return 0;    
 }
 
