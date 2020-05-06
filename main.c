4570c1c502afb244d5287e918c13d5be2843512a
Parasar Thulasiram <p.thulasiram@mail.utoronto.ca>
2020-05-06 16:46:11 -0400

    Added comment on added line

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 int main() {
     double arr[] = {1.0, 2.0, 3.0, 4.0};
     double constant = 2.0;
+/*defining constant to divide result by.*/
     double result = average(4, arr)/constant;
     printf("Half of the average of 1, 2, 3 and 4 is: %.4f\n", result);
     return 0;    
