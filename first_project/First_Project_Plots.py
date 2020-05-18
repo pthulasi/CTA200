f49153a24137934277ab71e66fc314fb5b8d3187
pthulasi <64913292+pthulasi@users.noreply.github.com>
2020-05-18 14:47:19 -0500

    Upload files for project

---
+++ first_project/First_Project_Plots.py
@@ -0,0 +1,123 @@
+
+# coding: utf-8
+
+# #### Importing packages
+
+# In[1]:
+
+
+import numpy as np
+np.seterr(divide='ignore', invalid='ignore')
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+# #### Loading data
+
+# In[2]:
+
+
+z_cohdd = np.load('z_cohdd.npy')#transposed coherently dedispersed data
+print(z_cohdd.shape)
+
+
+# #### Removing RFI
+
+# In[3]:
+
+
+polx = z_cohdd[:,0,:]
+poly = z_cohdd[:,1,:]
+Iraw = np.real(polx)**2 + np.imag(polx)**2 + np.real(poly)**2 + np.imag(poly)**2
+
+for i in range(1024):
+    Iraw[i,:] -= np.nanmedian(Iraw[i,:])
+    Iraw[i,:] /= np.nanstd(Iraw[i,:])
+    Iraw[i,:] -= np.nanmean(Iraw[i,:])
+
+
+# #### Function to rebin data
+
+# In[4]:
+
+
+def rebin(matrix, xbin, ybin):
+
+    shape1=matrix.shape[0]//xbin
+    shape2=matrix.shape[1]//ybin
+    return np.nanmean(np.reshape(matrix[:shape1*xbin,:shape2*ybin],(shape1,xbin,shape2,ybin)),axis=(1,3))
+
+
+# #### Plot style
+
+# In[5]:
+
+
+font = {'family' : 'Times',
+        'size'   : 20}
+
+matplotlib.rc('font', **font)
+
+
+# #### General plot parameters
+
+# In[19]:
+
+
+ref = 4 # rebinning how many freq channels to one freq chan
+ret = 10 # for time
+
+rebin_I = rebin(Iraw,ref,ret)
+rebin_I_mt = np.nanmean(rebin_I,axis=0)
+
+t_arr = 2.56*1e-3*ret*np.arange(rebin_I.shape[1])
+f_arr = 800-400*np.arange(rebin_I.shape[0])/rebin_I.shape[0]
+
+vmax = np.nanmean(rebin_I)+4*np.nanstd(rebin_I)
+vmin = np.nanmean(rebin_I)-2*np.nanstd(rebin_I)
+
+
+# #### Plotting time-series
+
+# In[14]:
+
+
+plt.figure(figsize=(12,6))
+plt.plot(t_arr,rebin_I_mt)
+plt.xlabel('Time (ms)')
+plt.ylabel('Intensity (arbitrary unit)')
+plt.xlim(0,t_arr.max())
+plt.grid(True)
+#plt.savefig("timeseries.pdf")
+plt.show()
+
+
+# #### Plotting waterfall large width
+
+# In[20]:
+
+
+plt.figure(figsize=(12,12))
+plt.pcolormesh(t_arr, f_arr, rebin_I, vmax=vmax, vmin=vmin)
+plt.xlabel('Time (ms)')
+plt.ylabel('Freq (MHz)')
+plt.xlim(290,293)
+plt.colorbar()
+#plt.savefig("wf_3ms.pdf")
+plt.show()
+
+
+# #### Plotting waterfall small width
+
+# In[11]:
+
+
+plt.figure(figsize=(12,12))
+plt.pcolormesh(t_arr, f_arr, rebin_I, vmax=vmax, vmin=vmin)
+plt.xlabel('Time (ms)')
+plt.ylabel('Freq (MHz)')
+plt.xlim(291.4,291.8)
+plt.colorbar()
+#plt.savefig("wf_0.4ms.pdf")
+plt.show()
+
---
+++ first_project/assi.py
@@ -0,0 +1,74 @@
+#Import packages
+import numpy as np
+from baseband import vdif
+import astropy.units as u
+import glob
+from baseband.helpers import sequentialfile as sf
+import pyfftw.interfaces.numpy_fft as fftw
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+
+np.seterr(divide='ignore', invalid='ignore')
+from matplotlib import cm
+
+#Single file to be searched
+file = "/scratch/p/pen/hsiuhsil/daily_vdif_aro/B0531+21/81949485/0007605586.vdif"
+#Constants
+sample_rate = 400/1024*u.MHz
+ftop = 800 * u.MHz - 400*np.arange(1024)/1024 * u.MHz
+D = 4148.808 * u.s * u.MHz**2 * u.cm**3 / u.pc
+fref = 800. * u.MHz
+DM = 56.7546 * u.pc / u.cm**3
+dt = 2.56e-6 * u.s
+
+wrap_time = D*(1/(ftop[-1]**2)-1/(ftop[0]**2))*DM
+wrap = int(np.ceil(D*(1/(ftop[-1]**2)-1/(ftop[0]**2))*DM/dt))
+print(wrap)
+dd_duration = 0.5*u.s
+ntime = int((wrap_time+dd_duration)/dt)
+nchan = 1024
+npol = 2
+
+#Opening single file
+fh = vdif.open(file, 'rs', sample_rate = sample_rate)
+print(fh.info())
+
+#Creating a list of file to be synthesized.
+filenames = sorted(glob.glob('/scratch/p/pen/hsiuhsil/daily_vdif_aro/B0531+21/81949485/*.vdif'))
+
+#Synthesizing files
+fraw = sf.open(filenames, 'rb')
+fh2 = vdif.open(fraw, 'rs', sample_rate = sample_rate)
+
+#matrix containing raw data
+z = fh2.read(ntime).astype(np.complex64)
+
+def coherent_dedispersion(z, DM, channel, axis=0):
+    """Coherently dedisperse signal."""
+
+    fcen = ftop[channel]
+
+    f = fcen + np.fft.fftfreq(z.shape[axis], dt)
+    dang = D * DM * u.cycle * f * (1./fref - 1./f)**2
+    with u.set_enabled_equivalencies(u.dimensionless_angles()):
+        dd_coh = np.exp(dang * 1j).conj().astype(np.complex64).value
+    if z.ndim > 1:
+        ind = [np.newaxis] * z.ndim
+        ind[axis] = slice(None)
+    if z.ndim > 1: 
+        dd_coh = dd_coh[ind]
+    z = fftw.fft(z, axis=axis)
+    z = fftw.ifft(z * dd_coh, axis=axis)
+    return z
+
+#Applying coherent de-dispersion
+for channel in range(nchan):
+    if channel%100==0:
+        print('channel:',channel)
+    z[..., channel] = coherent_dedispersion(z[..., channel], DM, channel)
+
+z_cohdd = z[:-wrap].transpose(2,1,0)
+#np.save('/home/p/pen/pthulasi/cita_surp_2020/cta200_project/z_cohdd.npy', z_cohdd)
+print(z_cohdd.shape, "saved, done")
+
